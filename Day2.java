caeac5f47cdde4207f223a3c4f9359ac12637cf8
Anushka_14 <anushkadeokar0614@gmail.com>
2026-07-22 19:27:54 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,31 @@
+// input output , vaariables, 
+import java.util.*;
+
+public class Day2 {
+    public static void main(String args[])
+    {
+        //variables
+    //    int a= 10;
+    //    int b = 25;
+    //    int sum = a + b;
+    //    System.out.println(sum);
+    //    int diff = a-b;
+    //    System.out.println(diff);
+      // input
+      //single word input
+      Scanner sc = new Scanner(System.in);
+      /* String name = sc.next();
+      System.out.println("name"+ name);
+
+      // multiple word input
+      String name1 = sc.nextLine();
+      System.out.println("Whole Line"+ name1);
+      //other type nextInt(), nextFloat(), nextDouble(), nextBoolean()
+*/
+     int a = sc.nextInt();
+     int b= sc.nextInt();
+     int sum = a+b;
+     System.out.println(sum);
+    
+    }
+}
